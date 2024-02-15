09b5cf670034547c3c48082a33c0bea9b5388e41
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-02-15 15:18:08 -0500

    Update from class 2/15

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
-/* B"H
-*/
+const express = require('express');
 
-console.log('Hello World')
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => (
+    res.send('Hello World!')
+));
+
+app.listen(PORT, () => (
+    console.log('App listening at http://localhost:$(PORT)')
+));
