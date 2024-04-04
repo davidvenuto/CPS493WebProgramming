e37ff41068a92e802782899aec4aaac6fbfec837
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-04-04 14:05:36 -0400

    Class 4/1

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const users = require('./controllers/users');
+/* B"H
+*/
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello New Paltz!')
-})
-.use('/api/v1/users', users)
+app
+  .use(express.json())
+
+app
+  .get('/', (req, res) => {
+    res.send('Hello New Paltz!')
+  })
+  .use('/api/v1/users', users)
+
+// Error handling
 
 
 app.listen(PORT, () => {
--- server/models/users.js
+++ server/models/users.js
@@ -1,19 +1,78 @@
-const data = require('../data/users.json')
+/* B"H
+*/
+const data = require('../data/users.json');
 
-function getAll(){
-    return data.items;
+/**
+ * @typedef {import('../../client/src/model/users').User} User
+ * */
+
+/**
+ * @returns {User[]}
+ * */
+function getAll() {
+    return data.items.map(x=> ({
+        ...x, password: undefined, bank: undefined, ssn: undefined,
+    }))
 }
 
-function get(id){
+/**
+ * @param {integer} id
+ * @returns {User}
+ * */
+function get(id) {
     return data.items.find(item => item.id == id);
 }
+
+/**
+ * @param {string} q
+ * @returns {User[]}
+ * */
 function search(q) {
-    return data.items.filter(item => 
+    return getAll().filter(item => 
         new RegExp(q, 'i').test(item.firstName) ||
         new RegExp(q, 'i').test(item.lastName) ||
-        new RegExp(q, 'i').test(item.email));
+        new RegExp(q, 'i').test(item.email) );
+}
+
+/**
+ * @param {User} user
+ * @returns {User}
+ * */
+function add(user) {
+    user.id = data.items.length + 1;
+    data.items.push(user);
+    return user;
+}
+
+/**
+ * @param {User} user
+ * @returns {User}
+ * */
+function update(user) {
+    const index = data.items.findIndex(item => item.id == user.id);
+    if (index >= 0) {
+        data.items[index] = {
+            ...data.items[index],
+            ...user
+        };
+        return user;
+    }
+    return null;
+}
+
+/**
+ * @param {integer} id
+ * @returns {boolean}
+ * */
+function remove(id) {
+    const index = data.items.findIndex(item => item.id == id);
+    if (index >= 0) {
+        data.items.splice(index, 1);
+        return true;
+    }
+    return false;
 }
 
 module.exports = {
-    getAll, get, search
+    getAll, get, search, add, update, remove
 }
