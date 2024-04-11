987c56c484387af10255d91818de99a9b5f33df6
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-04-11 14:08:37 -0400

    Updated for past 2 classes

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,60 @@
 /* B"H
 */
-const data = require('../data/users.json');
+const fs = require('fs');
+
+const fileName = __dirname + '/../data/users.json';
+
+/** @type { { items: User[] } } */
+let data //= require('../data/users.json');
+
+function isFileAccessible(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.access(fileName, fs.constants.F_OK, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+function readFile(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, 'utf8', (err, content) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(content);
+        });
+    });
+}
+
+function writeFile(fileName, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, content, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+isFileAccessible(fileName)
+    .then(() => readFile(fileName))
+    .then(content => {
+        data = JSON.parse(content);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+function save() {
+    return writeFile(fileName, JSON.stringify( data , null, 2) );
+}
 
 /**
  * @typedef {import('../../client/src/model/users').User} User
@@ -16,7 +70,7 @@ function getAll() {
 }
 
 /**
- * @param {integer} id
+ * @param {number} id
  * @returns {User}
  * */
 function get(id) {
@@ -41,6 +95,7 @@ function search(q) {
 function add(user) {
     user.id = data.items.length + 1;
     data.items.push(user);
+    save().catch(console.error);
     return user;
 }
 
@@ -55,22 +110,24 @@ function update(user) {
             ...data.items[index],
             ...user
         };
+        save().catch(console.error);
         return user;
     }
     return null;
 }
 
 /**
- * @param {integer} id
- * @returns {boolean}
+ * @param {number} id
+ * @returns {User | null}
  * */
 function remove(id) {
     const index = data.items.findIndex(item => item.id == id);
     if (index >= 0) {
-        data.items.splice(index, 1);
-        return true;
+        const deleted = data.items.splice(index, 1);
+        save().catch(console.error);
+        return deleted[0];
     }
-    return false;
+    return null;
 }
 
 module.exports = {
